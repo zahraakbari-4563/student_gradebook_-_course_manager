ccef2b87cb2bba354c08751a22c91a654007d8b7
zahraakbari-4563 <zahraakbari4563@gmail.com>
2026-07-12 15:06:26 +0430

    Created student class

---
+++ student.py
@@ -0,0 +1,31 @@
+class Student:
+    def __init__(self, student_id, name, email, courses):
+        self.__student_id = student_id
+        self.name = name
+        self.__email = email
+        self.courses = courses
+
+
+    def get_id(self):
+        return self.__student_id
+
+    def get_name(self):
+        return self.name
+
+    def set_email(self, email):
+        if "@" in email:
+            self.__email = email
+
+    def get_email(self):
+        return self.__email
+
+    def enroll_course(self, course_code):
+        self.courses.append(course_code)
+
+    def display_info(self):
+        print(f"Student ID: {self.__student_id}")
+        print(f"Name: {self.name}")
+        print(f"Email: {self.__email}")
+        print(f"Courses: {self.courses}")
+
+
