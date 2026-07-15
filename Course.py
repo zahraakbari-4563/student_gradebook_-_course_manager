e3d97fb21bafa73cfd50dc226677c9e0bde927f7
zahraakbari-4563 <zahraakbari4563@gmail.com>
2026-07-15 17:40:50 +0430

    Created course class

---
+++ Course.py
@@ -0,0 +1,30 @@
+class Course:
+    def __init__(self, course_code, course_name):
+        self.course_code = course_code
+        self.course_name = course_name
+        self.students = []
+        self.assessments = []
+
+    def add_student(self, student_id):
+        self.students.append(student_id)
+
+    def add_assessment(self, assessment):
+        self.assessments.append(assessment)
+
+    def find_assessment(self, title):
+        for assessment in self.assessments:
+            if assessment.title == title:
+                return assessment
+            else:
+                print("Assessment not found")
+        return None
+
+    def display_info(self):
+        print(f"Course Code: {self.course_code} " )
+        print(f"Course Name: {self.course_name}")
+        print(f"Students: {self.students}")
+        print(f"Assessments:")
+        for assessment in self.assessments:
+            assessment.display_info()
+
+
