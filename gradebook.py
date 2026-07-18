f9bd28a3821296ba37966b13479c1bdcc6e46d00
zahraakbari-4563 <zahraakbari4563@gmail.com>
2026-07-18 16:46:08 +0430

    Updated gradebook class
    Created another two methods to enroll student in a course and add assessment to a specific course.

--- gradebook.py
+++ gradebook.py
@@ -11,3 +11,17 @@ class GradeBook:
 
     def add_course(self, course):
         self.courses[course.course_code] = course
+
+
+    def enroll_student(self, student_id, course_code):
+        if course_code in self.courses and student_id in self.students:
+            self.courses[course_code].enroll_student(student_id)
+        else:
+            print("Course or student not found!")
+
+
+    def add_assessment(self, course_code, assessment):
+        if course_code in self.courses:
+            self.courses[course_code].add_assessment(assessment)
+        else:
+            print("Course not found.")
